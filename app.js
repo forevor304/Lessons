42b4f43f0691f04a5c16221d67a3d21582330ef0
forevor304 <fforest79@gmail.com>
2023-12-10 06:01:48 +0300

    Lesson 45 Node element and how we can use document

---
+++ app.js
@@ -0,0 +1,10 @@
+// console.log(document.documentElement);
+// console.log(document.body.childNodes);
+// console.log()
+// console.log(document.body.firstChild);
+// console.log(document.body.lastChild);
+
+// console.log(document.querySelector("#current").parentNode);
+console.log(document.querySelector("#current").parentElement);
+
+// console.log(document.querySelector('[data-current="3"]').nextElementSibling)
